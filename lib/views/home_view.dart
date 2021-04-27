8512d1d3e1dd3e789f0027154e0e76f26997d6b1
kaleb.silva <kaleb.silva@itbam.org.br>
2021-04-27 14:07:38 -0400

    refactor: change icon to marker

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -7,20 +7,48 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-
   Set<Marker> _markers = {};
+  BitmapDescriptor mapMarker;
+  BitmapDescriptor mapMarkerUnavailable;
+
+  @override
+  void initState() {
+    super.initState();
+    setCustomMarker();
+  }
+
+  void setCustomMarker() async {
+    BitmapDescriptor.fromAssetImage(
+            ImageConfiguration(size: Size(72, 72)), 'assets/available.png')
+        .then((onValue) {
+      mapMarker = onValue;
+    });
+
+    BitmapDescriptor.fromAssetImage(
+            ImageConfiguration(size: Size(72, 72)), 'assets/unavailable.png')
+        .then((onValue) {
+      mapMarkerUnavailable = onValue;
+    });
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     setState(() {
       _markers.add(
         Marker(
-          infoWindow: InfoWindow(
-            title: "NEXT MY HOUSE",
-            snippet: "TARUMA MANAUS TOP CENTER"
-          ),
-          markerId: MarkerId("id-1"),
-          position: LatLng(-3.006669087006096, -60.036741948623686),
-        ),
+            markerId: MarkerId("id-1"),
+            position: LatLng(-3.006669087006096, -60.036741948623686),
+            infoWindow: InfoWindow(
+                title: "NEXT MY HOUSE", snippet: "TARUMA MANAUS TOP CENTER"),
+            icon: mapMarker),
+      );
+
+      _markers.add(
+        Marker(
+            markerId: MarkerId("id-2"),
+            position: LatLng(-3.003152497680512, -60.0288825221795),
+            infoWindow:
+                InfoWindow(title: "ESTACIONAMENTO", snippet: "VENHA QUE É TOP"),
+            icon: mapMarkerUnavailable),
       );
     });
   }
@@ -31,12 +59,18 @@ class _HomeViewState extends State<HomeView> {
       appBar: AppBar(
         title: Text("Google Map"),
       ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        markers: _markers,
-        initialCameraPosition: CameraPosition(
-            target: LatLng(-2.9962554022094388, -60.02916501383111), zoom: 15),
-      ),
+      body: Stack(children: [
+        GoogleMap(
+          onMapCreated: _onMapCreated,
+          markers: _markers,
+          myLocationEnabled: true,
+          myLocationButtonEnabled: true,
+          zoomControlsEnabled: false,
+          initialCameraPosition: CameraPosition(
+              target: LatLng(-2.9962554022094388, -60.02916501383111),
+              zoom: 15),
+        ),
+      ]),
     );
   }
 }
